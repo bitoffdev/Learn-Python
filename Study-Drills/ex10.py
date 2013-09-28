9aa8095b8fc89946c2dc06c4ee4b64319eb47173
elliospizzaman <elliospizzaman@users.noreply.github.com>
2013-09-27 22:17:52 -0400

    Update ex10.py

--- Study-Drills/ex10.py
+++ Study-Drills/ex10.py
@@ -3,9 +3,9 @@
 
 #Using %r string modifier which is printing out the raw representation of what I typed
 print "%r \'" % "Single Quote Escape: "
-print "%r \"" % "Double Quote Escape: "
+print '%r \"' % "Double Quote Escape: "
 
 #Using %s string modifier which inserts modifier with Escape Sequences
 #This means the string will print correctly formatted
 print "%s \'" % "Single Quote Escape: "
-print "%s \"" % "Double Quote Escape: "
+print '%s \"' % "Double Quote Escape: "
