cabd11a934b7adc83eafe84737f3a07e0c545104
elliospizzaman <elliospizzaman@users.noreply.github.com>
2013-09-27 21:44:49 -0400

    Create ex10.py

---
+++ Study-Drills/ex10.py
@@ -0,0 +1,11 @@
+#This is study drill number 4 from exercise 10 of Learn Python the Hard Way
+#By: Elliot
+
+#Using %r string modifier which is printing out the raw representation of what I typed
+print "Single Quote Escape: %r" % "\'"
+print "Double Quote Escape: %r" % "\""
+
+#Using %s string modifier which inserts modifier with Escape Sequences
+#This means the string will print correctly formatted
+print "Single Quote Escape: %s" % "\'"
+print "Double Quote Escape: %s" % "\""
