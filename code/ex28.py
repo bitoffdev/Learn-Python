0a21895168c1661e9509541919f548035ae5b174
elliospizzaman <elliospizzaman@users.noreply.github.com>
2013-11-07 12:56:24 -0500

    Update ex28.py

--- code/ex28.py
+++ code/ex28.py
@@ -19,6 +19,12 @@ logic = [
         ["1 != 0 and 2 == 1", 1!=0 and 2==1],
         ["\"test\" == 1", "test" == 1],
         ["not (True and False)", not (True and False)],
-        ["not (1==1 and 0!=1)", not (1==1 and 0!=1)]
+        ["not (1==1 and 0!=1)", not (1==1 and 0!=1)],
+        ["not (10 == 1 or 1000 == 1000)", not (10 == 1 or 1000 == 1000)],
+        ["not (1 != 10 or 3 == 4)", not (1 != 10 or 3 == 4)],
+        ["not (\"testing\" == \"testing\" and \"Zed\" == \"Cool Guy\")", not ("testing" == "testing" and "Zed" == "Cool Guy")],
+        ["1 == 1 and not (\"testing\" == 1 or 1 == 0)", 1 == 1 and not ("testing" == 1 or 1 == 0)],
+        ["\"chunky\" == \"bacon\" and not (3 == 4 or 3 == 3)", "chunky" == "bacon" and not (3 == 4 or 3 == 3)],
+        ["3 == 3 and not (\"testing\" == \"testing\" or \"Python\" == \"Fun\")", 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")]
         ]
 table(logic)
